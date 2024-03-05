9054b9c32fa0e7a44348ba01f92511bcff4c2050
samyukthasuresh2001 <135725563+samyukthasuresh2001@users.noreply.github.com>
2024-03-05 17:49:10 +0530

    Update Fire.js

--- src/Fire.js
+++ src/Fire.js
@@ -6,12 +6,12 @@ import 'firebase/compat/auth';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBg_5S73sFN1LJu_bkaD8wJ1BKnxKcd8nQ",
+    apiKey: "****************************",
     authDomain: "netflix-clone-b9c14.firebaseapp.com",
     projectId: "netflix-clone-b9c14",
     storageBucket: "netflix-clone-b9c14.appspot.com",
     messagingSenderId: "27614060313",
-    appId: "1:27614060313:web:44ab1148712fa8426aef88"
+    appId: "****************************"
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
